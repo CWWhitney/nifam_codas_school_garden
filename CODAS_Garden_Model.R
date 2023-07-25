a5448c2b7f241f4931d85066df263f3bc639ff2d
dohoa1190 <dohoa1190@gmail.com>
2023-07-25 11:05:59 +0700

    initial cost model

--- CODAS_Garden_Model.R
+++ CODAS_Garden_Model.R
@@ -11,5 +11,20 @@ make_variables(estimate_read_csv(paste("inputs_school_garden.csv",sep="")))
 
 # Model ####
 
-cost
+number_of_years <- 
+
+establishment_cost <- rep(0, number_of_years)
+maintenance_cost <- rep(0, number_of_years)
+
+establishment_cost[1]<- equipment_cost + #consider to use cut-off value based on land area and number of participants
+  construction_cost +  # labor cost (2-3 people/day) + machine cost to setup garden system
+  teacher_training_cost # cost for training teacher on gardening
+establishment_cost[2:5] <- 0
+
+maintenance_cost[1] <- 0
+maintenance_cost[2: 5]<- input_cost + #fertilizer, irrigation, electricity
+  maintaining_labor + teacher_salary_cost
+
+
+total_cost <- establishment_cost + maintenance_cost + educational_cost
 
