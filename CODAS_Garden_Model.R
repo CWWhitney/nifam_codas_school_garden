a84c59450335c7d7a0d904b111ad6dfb8aad19bf
CWWhitney <cory.whitney@gmail.com>
2023-07-25 09:33:18 +0700

    add start of model

---
+++ CODAS_Garden_Model.R
@@ -0,0 +1,15 @@
+# School gardens in Hanoi ####
+
+
+inputs_school_garden
+
+make_variables <- function(est,n=1)
+{x <- random(rho=est,n=n)
+for(i in colnames(x))assign(i, as.numeric(x[1,i]),envir=.GlobalEnv)}
+
+make_variables(estimate_read_csv(paste("inputs_school_garden.csv",sep="")))
+
+# Model ####
+
+
+
