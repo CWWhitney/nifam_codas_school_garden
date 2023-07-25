2bf6ee0f75aeb9bcbba1dac2f6a10875170455fa
dohoa1190 <dohoa1190@gmail.com>
2023-07-25 11:49:48 +0700

    additional code

--- CODAS_Garden_Model.R
+++ CODAS_Garden_Model.R
@@ -1,7 +1,5 @@
+library(decisionSupport)
 # School gardens in Hanoi ####
-
-
-inputs_school_garden
 
 make_variables <- function(est,n=1)
 {x <- random(rho=est,n=n)
@@ -11,10 +9,11 @@ make_variables(estimate_read_csv(paste("inputs_school_garden.csv",sep="")))
 
 # Model ####
 
-number_of_years <- 
+#Cost####
 
 establishment_cost <- rep(0, number_of_years)
 maintenance_cost <- rep(0, number_of_years)
+harvest_value <- rep(0, number_of_years)
 
 establishment_cost[1]<- equipment_cost + #consider to use cut-off value based on land area and number of participants
   construction_cost +  # labor cost (2-3 people/day) + machine cost to setup garden system
@@ -22,9 +21,17 @@ establishment_cost[1]<- equipment_cost + #consider to use cut-off value based on
 establishment_cost[2:5] <- 0
 
 maintenance_cost[1] <- 0
-maintenance_cost[2: 5]<- input_cost + #fertilizer, irrigation, electricity
+maintenance_cost[2:5]<- input_cost + #fertilizer, irrigation, electricity
   maintaining_labor + teacher_salary_cost
 
 
-total_cost <- establishment_cost + maintenance_cost + educational_cost
+total_cost <- establishment_cost + maintenance_cost
+
+#Benefit###
+harvest_value <- if (if_school_has_canteen) {
+  harvest_value = canteen_saving
+} else {
+  harvest_value = sale_of_yield
+}
+
 
